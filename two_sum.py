fefc91d868722d362889c519bf6aff1e83df3acc
statjinx <statjinx@163.com>
2017-04-24 13:01:27 +0800

    two_sum

--- two_sum.py
+++ two_sum.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+#https://leetcode.com/problems/two-sum/#/description
+class Solution(object):
+    def twoSum( selfnums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+            m=0
+            for i in nums:
+                m=m+1
+                n=0
+                for j in nums:
+                    n=n+1
+                    if m!=n:
+                        if i+j==target:
+                            ind=[m-1,n-1]
+                            return ind
